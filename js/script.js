ea23083cbeb7df21e8d69cba62d797fcb592b73d
Diacon1985 <74572249+Diacon1985@users.noreply.github.com>
2026-06-18 11:45:02 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function writeMessage() {
+    const email = "service.itech@gmail.com"
+    const subject = encodeURIComponent(
+        "Interested in adopting"
+    );
+
+    const body = encodeURIComponent(
+        "Hello,\n\n"+
+        "I am interested in adopting one of the kittens\n\n" + 
+        "Please provide more information.\n\n" + 
+        "Thank you."
+    );
+
+    window.location.href = `mailto:${email}?subject=${body}&body=${body}`;
